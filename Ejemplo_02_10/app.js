0b2420f693f500e37bf0bf0850f4808ed1bfbf0f
gonself <alfonso.rodriguezllorente@gmail.com>
2021-10-19 23:37:35 +0200

    Ejemplo básico de creación de módulo

---
+++ Ejemplo_02_10/app.js
@@ -0,0 +1,3 @@
+const modulo = require('./modulo');
+
+console.log(modulo.getSaludo(new Date()));
---
+++ Ejemplo_02_10/modulo.js
@@ -0,0 +1,30 @@
+module.exports.getSaludo = (date) => {
+    if (date === undefined || _parameterValid === false) {
+        return 'Hola!';
+    }
+
+    const hour = date.getHours();
+
+    if (hour >= 7 && hour < 10) {
+        return 'Buenos días';
+    }
+    if (hour >= 10 && hour < 13) {
+        return 'Hola, ¿cómo va la mañana?';
+    }
+    if (hour >= 13 && hour < 15) {
+        return 'Hola, ¿vamos a comer?';
+    }
+    if (hour >= 15 && hour < 16) {
+        return 'Buenas tardes, ¿tomamos un café?';
+    }
+    if (hour >= 16 && hour < 20) {
+        return 'Buenas tardes'
+    }
+    if (hour >= 20) {
+        return 'Buenas noches';
+    }
+}
+
+function _parameterValid(date) {
+    return date instanceof Date;
+}
