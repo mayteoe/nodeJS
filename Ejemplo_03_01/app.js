bb54fa85f0760b2fd8cd4ef3adc670587130b642
gonself <alfonso.rodriguezllorente@gmail.com>
2021-10-20 18:17:13 +0200

    Se añaden nueva ruta POST y GET

--- Ejemplo_03_01/app.js
+++ Ejemplo_03_01/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const fs = require('fs');
 const cities = require('cities.json');
-const { response } = require('express');
 
 const app = express();
 const port = 8080;
@@ -22,6 +20,20 @@ app.get('/getByCountry/:country', (req, res) => {
 app.get('/getByName/:name', (req, res) => {
     let name = req.params.name;
     let response = cities.filter(city => city.name.toLowerCase().includes(name))
+    res.json(response);
+});
+
+app.post('/getByName/:name', (req, res) => {
+    let name = req.params.name;
+    let response = cities.filter(city => city.name.toLowerCase().includes(name))
+    res.json(response);
+});
+
+app.get('/getCities/:country/:name', (req, res) => {
+    let name = req.params.name;
+    let country = req.params.country;
+
+    let response = cities.filter(city => city.country == country && city.name.toLowerCase().includes(name))
     res.json(response);
 });
 
